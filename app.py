c7fe94452186567a2224a175e6d73f09735e0181
shaadali1810 <shaadali00786@gmail.com>
2023-03-24 01:54:29 +0530

    first commit

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")  
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
+    
+
